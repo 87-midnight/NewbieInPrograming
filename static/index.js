0ded21e49c0b57bb284bfafa789b3c8595fc847e
AnnieCattice <AnnieCattice@hotmail.com>
2019-10-28 10:52:24 +0800

    菜单树递归输出

--- static/index.js
+++ static/index.js
@@ -2,8 +2,24 @@ hljs.initHighlightingOnLoad();
 
 var localstorage;
 
-var menu_tree = function(){
+//递归树遍历菜单
+var menuTree = function(tree){
+    var str = "<ul class='menu-list'>%s</ul>";
+    var list = "";
+    for(var item in tree){
+        var li = "<li>";
+        if(tree[item].children.length>0){
+            li += menuTree(tree[item].children);
+        }
+        li += "<span>"+tree[item].name+"</span></li>";
+        list = list + li;
+    }
+    str = str.replace(/%s/g,list);
+    return str;
 };
+
+document.getElementById("menu").innerHTML = menuTree(files);
+
 var rendererMD = new marked.Renderer();
 marked.setOptions({
     renderer: rendererMD,
