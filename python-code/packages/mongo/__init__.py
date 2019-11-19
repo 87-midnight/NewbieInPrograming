8fd5bf096730f67f8599c17d4222bd0df2a045fd
AnnieCattice <AnnieCattice@hotmail.com>
2019-11-19 10:05:24 +0800

    MongoDB新增数据

---
+++ python-code/packages/mongo/__init__.py
@@ -0,0 +1,1 @@
+from .mongo_operate import *
--- python-code/packages/mongo/mongo_operate.py
+++ python-code/packages/mongo/mongo_operate.py
@@ -7,3 +7,15 @@ def database_info():
     db = pymongo.MongoClient('mongodb://localhost:27017/')
     db_list = db.list_database_names()
     print("数据库列表:", db_list)
+    return db_list
+
+
+def insert_data(db, user):
+    if user is None:
+        user = {"name": "test", "age": 17}
+    result = db["user"].insert_one(user)
+    print("插入数据结果:", result)
+
+
+if __name__ == '__main__':
+    database_info()
