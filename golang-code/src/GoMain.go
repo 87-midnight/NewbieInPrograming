8c63dccffafc6fad34a15bc1f38f42a9795541e1
AnnieCattice <AnnieCattice@hotmail.com>
2019-11-19 15:59:22 +0800

    for循环基础使用

--- golang-code/src/GoMain.go
+++ golang-code/src/GoMain.go
@@ -1,7 +1,10 @@
 package main
 
 import "./helloWorld"
+import "./loop"
 
 func main() {
 	helloWorld.PrintWellcome()
+	helloWorld.Start()
+	loop.LoopUnite()
 }
--- golang-code/src/helloWorld/Helloworld.go
+++ golang-code/src/helloWorld/Helloworld.go
@@ -5,3 +5,10 @@ import "fmt"
 func PrintWellcome() {
 	fmt.Printf("hello world,i'm coming...")
 }
+func Start() {
+	fmt.Print("start again")
+	end()
+}
+func end() {
+	fmt.Print("私有？？")
+}
