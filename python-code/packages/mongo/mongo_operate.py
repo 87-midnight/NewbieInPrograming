fccdc653390a330f6ab8bdbaa8c8d38c05e07063
AnnieCattice <AnnieCattice@hotmail.com>
2019-11-19 10:36:58 +0800

    查询文档列表

--- python-code/packages/mongo/mongo_operate.py
+++ python-code/packages/mongo/mongo_operate.py
@@ -1,21 +1,28 @@
 # -*- coding:utf8 -*-
 
 import pymongo
+import json
 
 
 def database_info():
     db = pymongo.MongoClient('mongodb://localhost:27017/')
     db_list = db.list_database_names()
     print("数据库列表:", db_list)
-    return db_list
+    return db
 
 
 def insert_data(db, user):
-    if user is None:
-        user = {"name": "test", "age": 17}
     result = db["user"].insert_one(user)
     print("插入数据结果:", result)
 
 
+def query_data(db, list_name):
+    result = db[list_name].find()
+    for item in result:
+        print("查询结果:", item)
+
+
 if __name__ == '__main__':
-    database_info()
+    db = database_info()
+    insert_data(db['local'], user={"name": "test", "age": "111"})
+    query_data(db['local'], 'user')
