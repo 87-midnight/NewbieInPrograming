28f6de6b5619f816b21aa1003b9954f8370027e3
AnnieCattice <AnnieCattice@hotmail.com>
2019-11-20 17:40:39 +0800

    log循环体里打印

--- golang-code/src/loop/LoopTest.go
+++ golang-code/src/loop/LoopTest.go
@@ -1,12 +1,14 @@
 package loop
 
-import "fmt"
+import (
+	log "github.com/sirupsen/logrus"
+)
 
 func LoopUnite() {
 	var y int
 	a := 10
 	for y = 1; y < 10; y++ {
-		fmt.Println(y)
+		log.Info("hello,%d", y)
 	}
 	println(":=的作用是自动判断类型并赋值，不需要var定义,a=", a)
 }
