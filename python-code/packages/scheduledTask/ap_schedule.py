7c58a9ca3c28a26bffc6150a5ef2022d70836e4b
AnnieCattice <AnnieCattice@hotmail.com>
2019-11-24 03:40:29 +0800

    测试阻塞定时任务

--- python-code/packages/scheduledTask/ap_schedule.py
+++ python-code/packages/scheduledTask/ap_schedule.py
@@ -4,14 +4,18 @@
 # https://www.jianshu.com/p/403bcb57e5c2
 from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
-
+import random
 
 # 输出时间
 def job():
     print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 
+def job_2():
+    print(random.randint(0,999))
+
 # BlockingScheduler
 scheduler = BlockingScheduler()
-scheduler.add_job(job, "cron", day_of_week="1-5", hour=6, minute=30)
+scheduler.add_job(job, "interval", seconds=2)
+scheduler.add_job(job_2,"interval",seconds=6)
 scheduler.start()
