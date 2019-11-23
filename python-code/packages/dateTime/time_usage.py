4a66ee8e95c1347307e0f1d77b17daa38bcbda8f
AnnieCattice <AnnieCattice@hotmail.com>
2019-11-23 16:01:30 +0800

    通过os方式获取父级目录路径

--- python-code/packages/dateTime/time_usage.py
+++ python-code/packages/dateTime/time_usage.py
@@ -1,4 +1,12 @@
 import time
+import os,sys
+
+# 可以使用os.path获取当前文件的上级目录路径，这样也能访问同级目录的文件了
+root_dir = os.path.dirname(os.path.abspath('.'))
+sys.path.append(root_dir+"\\")
+print(sys.path)
+
+from loop import *
 
 
 def time_test():
@@ -9,3 +17,7 @@ def time_test():
     localtime1 = time.asctime(time.localtime(time.time()))
     print("本地时间:%s" % localtime1)
     print("格式化时间:%s" % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
+
+if __name__ == '__main__':
+    list_()
