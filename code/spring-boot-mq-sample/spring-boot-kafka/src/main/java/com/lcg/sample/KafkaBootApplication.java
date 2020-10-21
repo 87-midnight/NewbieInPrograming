7d8b61f744418efa699fcafec1017d823c588823
AnnieCattice <AnnieCattice@hotmail.com>
2020-10-21 19:47:27 +0800

    增加spring-boot-rabbit/kafka模块示例代码

---
+++ code/spring-boot-mq-sample/spring-boot-kafka/src/main/java/com/lcg/sample/KafkaBootApplication.java
@@ -0,0 +1,12 @@
+package com.lcg.sample;
+
+/**
+ * @author linchuangang
+ * @createTime 2020/10/21
+ **/
+public class KafkaBootApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ code/spring-boot-mq-sample/spring-boot-rabbit/src/main/java/com/lcg/sample/RabbitBootApplication.java
@@ -0,0 +1,11 @@
+package com.lcg.sample;
+
+/**
+ * @author linchuangang
+ * @createTime 2020/10/21
+ **/
+public class RabbitBootApplication {
+    public static void main(String[] args) {
+
+    }
+}
