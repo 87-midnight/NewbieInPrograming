e19db331a8bc095b1640fe19193278b7beda50ab
AnnieCattice <AnnieCattice@hotmail.com>
2020-10-23 01:28:35 +0800

    定义spring-cloud常用模块

---
+++ code/spring-cloud-sample/spring-cloud-nacos-dubbo-sample/dubbo-traditional-common/src/main/java/com/lcg/sample/BasePrintService.java
@@ -0,0 +1,11 @@
+package com.lcg.sample;
+
+/**
+ * dubbo传统service-reference调用方式，定义类
+ * @author linchuangang
+ * @createTime 2020/10/22
+ **/
+public interface BasePrintService {
+
+    String print(String param);
+}
---
+++ code/spring-cloud-sample/spring-cloud-nacos-dubbo-sample/file-service/src/main/java/com/lcg/sample/FileServiceApplication.java
@@ -0,0 +1,8 @@
+package com.lcg.sample;
+
+/**
+ * @author linchuangang
+ * @createTime 2020/10/22
+ **/
+public class FileServiceApplication {
+}
