5f141f82858485ca37252caefda83a24df38261a
AnnieCattice <AnnieCattice@hotmail.com>
2020-11-08 23:30:34 +0800

    修改多线程目录结构、序列化细节问题记录

--- code/clone-serializable-sample/src/main/java/com/lcg/sample/serializable/SerializeTest.java
+++ code/clone-serializable-sample/src/main/java/com/lcg/sample/serializable/SerializeTest.java
@@ -26,7 +26,19 @@ public class SerializeTest {
 
     }
 
+    @Setter
+    @Getter
+    public static class Classes implements Serializable{
+        private Teacher teacher;
+    }
+    @Setter
+    @Getter
+    public static class Teacher{
+        private String name;
+    }
+
     public static void main(String[] args)throws Exception {
+        //序列化、反序列化示例代码--start
         Student origin = new Student();
         origin.setSn("123456");
         User info = new User();
@@ -48,7 +60,29 @@ public class SerializeTest {
         }catch (Exception var1){
             var1.printStackTrace();
         }
+        //序列化、反序列化示例代码--end
 
+        //成员类不实现Serializable 序列化、反序列化示例代码--start
+        Teacher teacher = new Teacher();
+        teacher.setName("小红");
+        Classes classes = new Classes();
+        classes.setTeacher(teacher);
+        try(
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        ) {
+            //序列化，写入字节数组输出流
+            objectOutputStream.writeObject(classes);
+
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+            //反序列化，从字节数组输入流读取对象的字节数组，转换为一个新的对象，深拷贝
+            Classes copy = (Classes) objectInputStream.readObject();
+            System.out.println("地址比较结果："+ Objects.equals(classes,copy));
+        }catch (Exception var1){
+            //Teacher没有序列化，会报错
+            var1.printStackTrace();
+        }
 
     }
 }
